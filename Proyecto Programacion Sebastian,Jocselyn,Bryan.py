636659cdcf22e6b974853e78aa48fa4c4bf54fca
Jocselynchs <132708923+Jocselynchs@users.noreply.github.com>
2024-02-18 16:43:53 -0600

    Update Proyecto Programacion Sebastian,Jocselyn,Bryan.py

--- Proyecto Programacion Sebastian,Jocselyn,Bryan.py
+++ Proyecto Programacion Sebastian,Jocselyn,Bryan.py
@@ -1,2 +1,4 @@
 """Cadena de restaurantes Sabores Deliciosos"""
-
+""" Primer avance; Generar el primer módulo completo y funcional.
+• Generar el segundo módulo completo y funcional.
+• Generar el tercer módulo completo y funcional"""
