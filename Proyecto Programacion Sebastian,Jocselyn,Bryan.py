30fb02bf14f5c9224b3bb8d2653fc8ec0e8969e6
Jocselynchs <132708923+Jocselynchs@users.noreply.github.com>
2024-02-18 17:19:41 -0600

    Update Proyecto Programacion Sebastian,Jocselyn,Bryan.py

--- Proyecto Programacion Sebastian,Jocselyn,Bryan.py
+++ Proyecto Programacion Sebastian,Jocselyn,Bryan.py
@@ -3,4 +3,9 @@
 • Generar el segundo módulo completo y funcional.
 • Generar el tercer módulo completo y funcional"""
 
-print("Bienvenido ala cadena de restaurantes Sabores Deliciosos")
+print("Bienvenido a la cadena de restaurantes Sabores Deliciosos")
+
+menu1=input("¿Desea ingresar al menu?\n Marque Si para ingresar al menu o No para salir del menu")
+while menu1==Si:
+    print("Este es su menu")
+    print
